6663f1afb6f378e762ddace5347dd3339afa84a8
Christian Avendaño <chrisavendano0508@gmail.com>
2025-10-04 20:23:23 +0200

    Day 4 part 2

--- day4/2.py
+++ day4/2.py
@@ -1,5 +1,25 @@
 '''
 URL: https://adventofcode.com/2024/day/4
-answer = ???
-
+answer = 2029
 '''
+
+def count_x_mas(grid):
+    R, C = len(grid), len(grid[0])
+    cnt = 0
+    for r in range(1, R - 1):
+        for c in range(1, C - 1):
+            if grid[r][c] != 'A':
+                continue
+            diag1 = grid[r - 1][c - 1] + grid[r + 1][c + 1]
+            diag2 = grid[r - 1][c + 1] + grid[r + 1][c - 1]
+            if (diag1 in ('MS', 'SM')) and (diag2 in ('MS', 'SM')):
+                cnt += 1
+    return cnt
+
+def main():
+    with open("input.txt") as f:
+        grid = f.read().splitlines()
+    print(count_x_mas(grid))
+
+if __name__ == "__main__":
+    main()
